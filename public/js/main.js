24d6c0825308811a4f98cbe4ce1b6aaec9c04108
mrCaelum <louis.kleiver@gmail.com>
2022-03-01 20:47:24 +0100

    Add retirement investment

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,10 @@
 const ctxTimeline = document.getElementById('graphTimeline');
 let collapsables = [];
 
-const labels = [
-	'January',
-	'February',
-	'March',
-	'April',
-	'May',
-	'June',
-];
-
 const timeline = new Chart(ctxTimeline, {
   type: 'line',
   data: {
-		labels: labels,
+		labels: [0, 1, 2, 3, 4, 5, 6],
 		datasets: [{
 			label: 'Timeline',
 			data: [0, 0, 0, 0, 0, 0, 0],
@@ -41,15 +32,22 @@ function updateCollapse(id) {
 function refresh () {
 	const starting_value = document.getElementById('starting-value').value || 0;
 	let values = [];
-	for (let i = 0; i < 7; ++i) {
-		values.push(starting_value + i);
-	}
-	timeline.data.datasets[0].data = values;
-	for (let i = 0; i < collapsables.length; ++i) {
-		if (collapsables[i].selected) {
-			// UPDATE
+	if (collapsables[2].selected) {
+		const quantity = document.getElementById('invest3-quantity').value || 0;
+		const duration = document.getElementById('invest3-duration').value || 0;
+		const duration_type = document.getElementById('invest3-duration-label').value || 1;
+		let labels = [];
+		for (let i = 0; i < duration; ++i) {
+			labels.push(i);
+			values.push(starting_value + (quantity * i));
+		}
+		timeline.data.labels = labels;
+	} else {
+		for (let i = 0; i < 7; ++i) {
+			values.push(starting_value + i);
 		}
 	}
+	timeline.data.datasets[0].data = values;
 	timeline.update();
 };
 
